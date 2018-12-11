73bc33b0abf69ef68a59c677d46e76e9a4c2eb98
Javier Ramirez <apollo.infinity88@gmail.com>
2018-12-11 10:30:41 -0800

    Cleaned up code so that it can utilize the MVC pattern more efficiently

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,17 @@
-const path = require('path');
+const path = require('path'); // Core NodeJS module
 
 const express = require('express');
 
+// const rootDir = require('../util/path');
 const productsController = require('../controllers/products');
 
 const router = express.Router();
 
 
-// /admin/add-product => GET
+
+
 router.get('/add-product', productsController.getAddProduct);
 
-// /admin/add-product => POST
 router.post('/add-product', productsController.postAddProduct);
 
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -6,6 +6,7 @@ const productsController = require('../controllers/products')
 
 const router = express.Router();
 
+
 router.get('/', productsController.getProducts);
 
 module.exports = router;
