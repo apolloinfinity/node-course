dc6e8ad3d971ba69185d0bc0bba495b7158f74f4
Javier Ramirez <apollo.infinity88@gmail.com>
2019-01-07 07:27:22 -0800

    Saving progress

--- models/product.js
+++ models/product.js
@@ -15,8 +15,11 @@ const getProductsFromFile = cb => {
 }
 
 module.exports = class Product {
-    constructor(t) {
-        this.title = t;
+    constructor(title, imgURL, description, price) {
+        this.title = title;
+        this.imgUrl = imgURL;
+        this.description = description;
+        this.price = price;
     }
 
     save() {
