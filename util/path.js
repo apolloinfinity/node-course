ed8ba540692e8b3f3665aaa674a9bf46250006d7
Javier Ramirez <apollo.infinity88@gmail.com>
2020-03-09 15:08:21 -0700

    Updated some files due to whitespace and missing semi-colons. Cart.json is the result of a new Post route

--- util/path.js
+++ util/path.js
@@ -1,3 +1,3 @@
 const path = require('path');
 
-module.exports = path.dirname(process.mainModule.filename) // Gives the path to the main file that makes the app run
+module.exports = path.dirname(process.mainModule.filename); // Gives the path to the main file that makes the app run
