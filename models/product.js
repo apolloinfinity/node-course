8df73cf430bd858552741c27fa314db890a43964
Javier Ramirez <apollo.infinity88@gmail.com>
2020-04-03 10:02:44 -0700

    Updated packages because of vulnerabilitiesand saving current state of project

--- models/product.js
+++ models/product.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const { throws } = require('assert');
 
 const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
 
@@ -14,28 +15,39 @@ const getProductsFromFile = (cb) => {
 };
 
 module.exports = class Product {
-	constructor(title, imageUrl, description, price) {
+	constructor (id, title, imageUrl, description, price) {
+		this.id = id;
 		this.title = title;
 		this.imageUrl = imageUrl;
 		this.description = description;
 		this.price = price;
 	}
 
-	save() {
+	save () {
 		this.id = Math.random().toString();
 		getProductsFromFile((products) => {
-			products.push(this);
-			fs.writeFile(p, JSON.stringify(products), (err) => {
-				console.log(err);
-			});
+			if (this.id) {
+				const existingProduct = product.findIndex((prod = prod.id === this.id));
+				const updatedProducts = [ ...products ];
+				updatedProducts[existingProduct] = this;
+				fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+					console.log(err);
+				});
+			} else {
+				this.id = Math.random().toString();
+				products.push(this);
+				fs.writeFile(p, JSON.stringify(products), (err) => {
+					console.log(err);
+				});
+			}
 		});
 	}
 
-	static fetchAll(cb) {
+	static fetchAll (cb) {
 		getProductsFromFile(cb);
 	}
 
-	static findById(id, cb) {
+	static findById (id, cb) {
 		getProductsFromFile((products) => {
 			const product = products.find((p) => p.id === id);
 			cb(product);
