33c5411a583301d9bcc5e874bebddfd538fefb72
Javier Ramirez <apollo.infinity88@gmail.com>
2020-04-22 08:27:21 -0700

    Added delete product to cart.js, and added a statitc method to the product class to delete by id

--- models/product.js
+++ models/product.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
-const { throws } = require('assert');
+
+const Cart = require('./cart');
 
 const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
 
@@ -24,7 +25,6 @@ module.exports = class Product {
 	}
 
 	save () {
-		this.id = Math.random().toString();
 		getProductsFromFile((products) => {
 			if (this.id) {
 				const existingProductIndex = products.findIndex((prod) => prod.id === this.id);
@@ -42,6 +42,19 @@ module.exports = class Product {
 			}
 		});
 	}
+
+	static deleteById (id) {
+		getProductsFromFile((products) => {
+			const product = products.find((prod) => prod.id === id);
+			const updatedProducts = products.filter((prod) => prod.id !== id);
+			fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+				if (!err) {
+					Cart.deleteProduct(id, product.price);
+				}
+			});
+		});
+	}
+
 	static fetchAll (cb) {
 		getProductsFromFile(cb);
 	}
