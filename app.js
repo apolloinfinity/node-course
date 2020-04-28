135ecdfccf89aec52a1626e302ea14decb92ff90
Javier Ramirez <apollo.infinity88@gmail.com>
2020-04-28 09:25:52 -0700

    Added sequelize to project

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const errorController = require('./controllers/error');
+const sequelize = require('./util/database');
 
 const app = express();
 
@@ -13,6 +15,7 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -20,5 +23,12 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+
+sequelize
+  .sync()
+  .then((result) => {
+    // console.log(result);
+  })
+  .catch((err) => console.log(err));
 
 app.listen(3000);
